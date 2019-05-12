b32f52304ff1d050a0afaf8f972d027e966feafd
Jefferson Fausto Vaz <faustovaz@gmail.com>
2019-05-11 22:02:53 -0300

    Added dinamic way to add console_scripts

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
+from os import listdir
+from os.path import dirname, abspath, isdir, isfile
+
+def scripts():
+    scripts = ['pyzz = pyzz.__init__:main']
+    pyzz_path = dirname(abspath(__file__)) + '/pyzz/'
+    for file in listdir(pyzz_path):
+        full_file_path = pyzz_path + file
+        if isdir(full_file_path) and isfile(full_file_path + '/__init__.py'):
+            scripts.append('{0} = pyzz.{0}.__init__:main'.format(file))
+    return scripts
 
 setup(
     name='pyzz',
     install_requires = ['docopt', 'pytest'],
     entry_points = {
-        'console_scripts' : [
-            'pyzz = pyzz.__init__:main'
-        ]
+        'console_scripts' : scripts()
     },
 )
