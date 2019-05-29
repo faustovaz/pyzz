1a4d753d463e7c1c6fd2ad60f959adf7e6f4dae1
Jefferson Fausto Vaz <faustovaz@gmail.com>
2019-05-28 23:45:53 -0300

    Changed method names

--- pyzz/pycpf/__init__.py
+++ pyzz/pycpf/__init__.py
@@ -15,22 +15,21 @@ from random import randint
 from functools import reduce
 from re import match
 
-def _generate_first_digit(sequence):
-    mapped_values = list(map(lambda x,y: x*y, list(range(10, 1, -1)), sequence))
+def _first_digit(sequence):
+    mapped_values = map(lambda x,y: x*y, list(range(10, 1, -1)), sequence)
     digit = abs((reduce(lambda x,y: x + y, mapped_values) % 11) - 11)
     return 0 if digit > 9 else digit
 
-def _generate_second_digit(sequence):
-    mapped_values = list(map(lambda x,y: x*y, list(range(11, 2, -1)), sequence))
-    digit = reduce(lambda x,y: x + y, mapped_values) \
-            + (_generate_first_digit(sequence) * 2)
+def _second_digit(sequence):
+    values = map(lambda x,y: x*y, list(range(11, 2, -1)), sequence)
+    digit = reduce(lambda x,y: x + y, values) + (_first_digit(sequence) * 2)
     digit = abs((digit % 11) - 11)
     return 0 if digit > 9 else digit
 
 def cpf():
     random_sequence = [randint(0,9) for i in range(9)]
-    random_sequence.append(_generate_first_digit(random_sequence))
-    random_sequence.append(_generate_second_digit(random_sequence))
+    random_sequence.append(_first_digit(random_sequence))
+    random_sequence.append(_second_digit(random_sequence))
     return ''.join(str(num) for num in random_sequence)
 
 def formattedCpf():
@@ -45,8 +44,8 @@ def is_a_valid_cpf(given_cpf):
     if is_a_valid_cpf_format(given_cpf):
         given_cpf = given_cpf.replace('.','').replace('-','')
         given_cpf = [int(d) for d in given_cpf]
-        first_digit = _generate_first_digit(given_cpf[0:-2])
-        second_digit = _generate_second_digit(given_cpf[0:-2])
+        first_digit = _first_digit(given_cpf[0:-2])
+        second_digit = _second_digit(given_cpf[0:-2])
         return given_cpf[-2:] == [first_digit, second_digit]
     return False
 
