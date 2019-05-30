1a593fa9c8c565d507122d493297b235adfc2d1f
Jefferson Fausto Vaz <jefferson.vaz@unila.edu.br>
2019-05-30 17:36:17 -0300

    Added is_valid_format method

--- pyzz/pydate/__init__.py
+++ pyzz/pydate/__init__.py
@@ -1,5 +1,5 @@
 """
-pydate - <Insert function description here>
+pydate - A date calculator
 Usage:
     <Usage here>
 Options:
@@ -7,12 +7,20 @@ Options:
 """
 from __future__ import print_function
 from docopt import docopt
+from re import match
+
+def is_valid_format(str_date):
+    br_standard = r'^\d{2}/\d{2}/\d{4}(\s\d{2}:\d{2}:\d{2})?$'
+    iso_standard = r'^\d{4}-\d{2}-\d{2}(\s\d{2}:\d{2}:\d{2})?$'
+    return match(br_standard, str_date) or match(iso_standard, str_date)
 
 def parse_date(formatted_date):
-    return 1
+    pass
+    
 
 def main():
-    args = docopt(__doc__)
+    #args = docopt(__doc__)
+    pass
 
 if __name__ == '__main__':
     main()
--- tests/pydate/test_pydate.py
+++ tests/pydate/test_pydate.py
@@ -2,5 +2,14 @@ import unittest
 from pyzz import pydate
 
 class TestParsingDate(unittest.TestCase):
-    def test_parse_date_brazilian_standard(self):
-        self.assertEqual(pydate.parse_date('2018-07-07'), 1)
+    def test_brazilian_date_formatt(self):
+        self.assertTrue(pydate.is_valid_format('27/11/1983'))
+        self.assertTrue(pydate.is_valid_format('26/05/2019 01:12:13'))
+        self.assertFalse(pydate.is_valid_format('27/11/83'))
+        self.assertFalse(pydate.is_valid_format('27/11/1983 01:12'))
+
+    def test_iso_date_format(self):
+        self.assertTrue(pydate.is_valid_format('1983-11-27'))
+        self.assertTrue(pydate.is_valid_format('2011-10-11 15:12:30'))
+        self.assertFalse(pydate.is_valid_format('83-11-27 15:15:15'))
+        self.assertFalse(pydate.is_valid_format('1983-11-27 15:15'))
