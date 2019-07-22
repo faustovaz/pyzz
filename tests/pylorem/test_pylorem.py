986045e581551922de37e82551a68aaecac39e6a
Jefferson Fausto Vaz <jefferson.vaz@unila.edu.br>
2019-07-22 18:23:12 -0300

    Removed response.msg due to not availability in python 2

--- tests/pylorem/test_pylorem.py
+++ tests/pylorem/test_pylorem.py
@@ -10,7 +10,6 @@ class TestAPIAvailability(unittest.TestCase):
 
     def test_if_api_is_working(self):
         response = request.urlopen('https://loripsum.net/api/plaintext/')
-        self.assertEquals('OK', response.msg)
         self.assertEquals(200, response.getcode())
 
     def test_if_api_is_generating_lorem_text(self):
