591225ed2d9aa23eb72a0db0386a014078d7b9a9
Jefferson Fausto Vaz <jefferson.vaz@unila.edu.br>
2019-10-17 17:47:56 -0300

    Added a install instruction file

---
+++ install.sh
@@ -0,0 +1,17 @@
+#apt-get update
+# apt-get install -y  python3.6 \
+#                     python3-venv \
+#                     python3-dev \
+#                     python3-setuptools \
+#                     libpq-dev \
+#                     build-essential \
+#                     binutils \
+#                     g++
+
+#rm -rf $HOME/.pyzz
+#mkdir $HOME/.pyzz
+#python3 -m venv $HOME/.pyzz/
+#$HOME/.pyzz/bin/pip install .
+#export PATH=$PATH:$HOME/.pyzz/bin/
+
+
