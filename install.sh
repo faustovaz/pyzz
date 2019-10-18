ded2968ed66f4f52d7128400ac034e9ff6b005da
Jefferson Fausto Vaz <jefferson.vaz@unila.edu.br>
2019-10-18 13:45:55 -0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,18 @@
 #                     binutils \
 #                     g++
 
-#rm -rf $HOME/.pyzz
-#mkdir $HOME/.pyzz
-#python3 -m venv $HOME/.pyzz/
-#$HOME/.pyzz/bin/pip install .
-#export PATH=$PATH:$HOME/.pyzz/bin/
+rm -rf $HOME/.pyzz
+mkdir $HOME/.pyzz
+python3 -m venv $HOME/.pyzz/
+$HOME/.pyzz/bin/pip install --upgrade pip
+$HOME/.pyzz/bin/pip install .
 
+#ZSHELL
+if [ -z "$(grep '$HOME/.pyzz/bin' $HOME/.localrc)" ]; then
+   echo 'export PATH=$HOME/.pyzz/bin/:$PATH' >> $HOME/.localrc
+fi
 
+#BASH
+if [ -z "$(grep '$HOME/.pyzz/bin' $HOME/.bashrc)" ]; then
+   echo 'export PATH=$HOME/.pyzz/bin/:$PATH' >> $HOME/.bashrc
+fi
